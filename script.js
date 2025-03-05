0db9cd03923f904d7249da4b6e2aa1da3a0b2928
Figurantye <eva01.gustavo@gmail.com>
2025-03-05 11:59:55 -0300

    finalizando script

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const carForm = document.getElementById("car-Form")
-    const carList = document.getElementById("car-list")
+    loadCars();
 
-    //Carregar carros do localStorage
-
-    const loadCars = () => {
-        const cars = JSON.parse(localStorage.getItem("cars")) || [];
-        carList.innerHTML = "";
-        cars.array.forEach((car, index) => addCarToList(car, index));
-    }
-
-    //Salvar carros no localStorage
-    const saveCars = (cars) => {
-        localStorage.setItem("cars", JSON.stringify(cars))
-    }
+    const carForm = document.getElementById("car-form")
+    carForm.addEventListener("submit", addCar)
 })
+    
+//Carregar carros do localStorage
 
-const model = document.getElementById("model")
-const year = document.getElementById("year")
-const price = document.getElementById("price")
+function loadCars(){
+    var carList = document.getElementById("car-list");
+    var cars = JSON.parse(localStorage.getItem("cars")) || [];
+    carList.innerHTML = "";
+    for (let i = 0; i < cars.length; i++) {
+        addCarToList(cars[i], i)
+    }
+}
+    
+//Adicionar carros à lista no front-end
+
+function addCarToList(car, index) {
+    var carList = document.getElementById("car-list");
+    var li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = "<span> <strong>" + car.model + "</strong> - " + car.year + " R$" + car.price + "</span>" + 
+    "<button class='btn btn-danger btn-sm' onclick='deleteCar(" + index + ")'>Deletar</button>"
+    
+    carList.appendChild(li);
+}
+
+//Adicionar um carro ao localStorage, usando a função saveCars
+
+function addCar(event) {
+    event.preventDefault();
+    
+    var model = document.getElementById("model").value;
+    var year = parseInt(document.getElementById("year").value);
+    var price = parseFloat(document.getElementById("price").value).toFixed(2);
+
+    var cars = JSON.parse(localStorage.getItem("cars")) || [];
+    cars.push({model: model, year: year, price: price});
+
+    saveCars(cars);
+    loadCars();
+
+}
+
+//Salvar carros, ou adicioná-los no localStorage
+function saveCars(cars) {
+    localStorage.setItem("cars", JSON.stringify(cars));
+}
